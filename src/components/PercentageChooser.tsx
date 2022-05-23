a2fc60ed8a9cb692ae695e221e69a9ec15b688f5
Kenyon Tu <kenyontu@outlook.com>
2022-05-23 17:21:42 +0800

    chore: remove unused import

--- src/components/PercentageChooser.tsx
+++ src/components/PercentageChooser.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react'
+import { useRef, useEffect } from 'react'
 import classNames from 'classnames'
 
 import styles from './PercentageChooser.module.css'
